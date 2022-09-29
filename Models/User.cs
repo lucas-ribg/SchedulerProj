71b5d22c44f721b5543ade9011dc58aae902233d
Lucas Ribeiro <lucas123.r.g@gmail.com>
2022-09-28 23:04:53 -0300

    atualizando modelos

--- Models/User.cs
+++ Models/User.cs
@@ -1,9 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace SchedulerProj.Models;
 
+[Table("users")]
 public class User 
 {
+    [Display(Name="Code")]
+    [Key]
+    [Column("idusers")]
     public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? Email { get; set; }
-    public string? Password { get; set; } 
+
+    [Display(Name="Name")]
+    [Column("name")]
+    public string Name { get; set; }
+
+    [Display(Name="E-mail")]
+    [EmailAddress]
+    [Column("email")]
+    public string Email { get; set; }
+
+    [Display(Name="Password")]
+    [DataType(DataType.Password)]
+    [Column("password")]
+    public string Password { get; set; } 
 }
