6c219816e4b4aceded66da9da7ceb0308f4d06bd
Lucas Ribeiro <lucas123.r.g@gmail.com>
2022-09-28 23:04:08 -0300

    criando controller de usuarios

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,64 +1,163 @@
-using SchedulerProj.Models;
-using SchedulerProj.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SchedulerProj.Data;
+using SchedulerProj.Models;
 
-namespace SchedulerProj.Controllers;
-
-[ApiController]
-[Route("[controller")]
-public class UserController : ControllerBase
+namespace SchedulerProj.Controllers
 {
-    public UserController() 
+    public class UserController : Controller
     {
+        private readonly Context _context;
+
+        public UserController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: User
+        public async Task<IActionResult> Index()
+        {
+              return _context.User != null ? 
+                          View(await _context.User.ToListAsync()) :
+                          Problem("Entity set 'Context.User'  is null.");
+        }
+
+        // GET: User/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.User == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: User/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Email,Password")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: User/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.User == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: User/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Email,Password")] User user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: User/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.User == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.User == null)
+            {
+                return Problem("Entity set 'Context.User'  is null.");
+            }
+            var user = await _context.User.FindAsync(id);
+            if (user != null)
+            {
+                _context.User.Remove(user);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(int id)
+        {
+          return (_context.User?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
     }
-
-    [HttpGet]
-    public ActionResult<List<User>> GetAll() =>
-        UserService.GetAll();
-
-    [HttpGet("{id}")]
-    public ActionResult<User> Get(int id)
-    {
-        var user = UserService.Get(id);
-        if (user == null)
-            return NotFound();
-
-        return user;
-    }
-
-    [HttpPost]
-    public IActionResult Create(User user)
-    {
-        UserService.Add(user);
-        return CreatedAtAction(nameof(Create), new { id = user.Id }, user);
-    }
-
-    [HttpPut("{id}")]
-    public IActionResult Update(int id, User user)
-    {
-        if (id != user.Id)
-            return BadRequest();
-
-        var existingUser = UserService.Get(id);
-        if (existingUser is null)
-            return NotFound();
-
-        UserService.Update(user);
-
-        return NoContent();
-    }
-
-    [HttpDelete("{id}")]
-    public IActionResult Delete(int id)
-    {
-        var user = UserService.Get(id);
-
-        if (user is null)
-            return NotFound();
-
-        UserService.Delete(id);
-
-        return NoContent();   
-    }
-
 }
